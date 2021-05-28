441a1a4ffdb51b0cf9d3f48e07b810c355121562
Tom <Tom.Cybart@hochschule-stralsund.de>
2021-05-28 06:45:40 -0400

    Start im Labyrinth, damit der Turtlebot im Start sich an eine erste Wand orientiert

--- src/StartStopServer.cpp
+++ src/StartStopServer.cpp
@@ -55,8 +55,8 @@ bool robot_move(const ROBOT_MOVEMENT move_type) {
         motor_command.angular.z = -0.05;
     } else if (move_type == GO_RIGHT) {
         ROS_INFO("Rechts! \n");
-        motor_command.linear.x = 0.05;
-        motor_command.angular.z = -0.05;
+        motor_command.linear.x = 0.025;
+        motor_command.angular.z = -0.08;
     } else if (move_type == GO_LEFT) {
         ROS_INFO("Links! \n");
         motor_command.linear.x = 0.0;
@@ -93,24 +93,24 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr &msg) {
 
     for (int k = 350; k < 360; k++) {
         summeVorne += laser_ranges[k];
-        if(k = 360){
-            averageVorne = summeVorne/21.0;
+        if (k == 359) {
+            averageVorne = summeVorne / 21.0;
         }
 
     }
 
     for (int z = 80; z < 101; z++) {
         summeLinks += laser_ranges[z];
-        if(z = 101){
-            averageLinks = summeLinks/21.0;
+        if (z == 100) {
+            averageLinks = summeLinks / 21.0;
         }
 
     }
 
     for (int z = 260; z < 281; z++) {
         summeRechts += laser_ranges[z];
-        if(z = 281){
-            averageRechts = summeRechts/21.0;
+        if (z == 280) {
+            averageRechts = summeRechts / 21.0;
         }
     }
 
@@ -118,53 +118,62 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr &msg) {
     ROS_INFO("averageRechts: %f", averageRechts);
     ROS_INFO("averageLinks: %f", averageLinks);
     ROS_INFO("averageVorne: %f", averageVorne);
-    if ( averageRechts > 0.35){
-        robot_move(GO_RIGHT);       //rechts fahren bis zur Wand
 
+    if (averageRechts && averageLinks > 0.5 && averageVorne >= 0.3) {
+        robot_move(FORWARD);
+    } else if (averageRechts && averageLinks > 0.5 && averageVorne <= 0.3) {
+        robot_move(TURN_LEFT);
     } else {
-        if (averageVorne > 0.35){
-            robot_move(FORWARD);    //da dicht genug an der rechten Wand fahr vorne
-        }
-        else if (averageVorne <= 0.25 && averageLinks > 0.25){
-            robot_move(GO_LEFT);    //da Vorne und Rechts dicht an wand , nach links fahren da Laserwerte keine Wand anzeigen
-        }
-        else if (averageVorne <= 0.25 && averageLinks <= 0.25){
-            robot_move(TURN_LEFT);  //da Vorne, Rechts und Links Laserwerte anzeigen das Wände sehr dicht sind, umdrehen
-        }
-    }
+        if (averageRechts > 0.35) {
+            robot_move(GO_RIGHT);       //rechts fahren bis zur Wand
 
-
-
-   /* for (size_t i = 0; i < range_size; i++) {
-        if (laser_ranges[i] < range_min) {
-            range_min = laser_ranges[i];
-        }*/
-
-
-        /*for (float x : msg->ranges) {  //geht die Laser Wertw in der For Schleife durch
-            if (x >= msg->range_min && x <= msg->range_max && x <=sensorEntfernung) { //wenn der x Wert größergleich ist als die min range und kleinergleich ist als die max range und
-                sensorEntfernung = x;     //kleiner gleich der SensorReichweite von 3,5 ist wird die sensorEntfernung auf x gesetzt
-            } else {        //ansonsten wird der Wert um 1 erhöht
-                counter += 1;
+        } else {
+            if (averageVorne > 0.35) {
+                robot_move(FORWARD);    //da dicht genug an der rechten Wand fahr vorne
+            } else if (averageVorne <= 0.25 && averageLinks > 0.25) {
+                robot_move(
+                        GO_LEFT);    //da Vorne und Rechts dicht an wand , nach links fahren da Laserwerte keine Wand anzeigen
+            } else if (averageVorne <= 0.25 && averageLinks <= 0.25) {
+                robot_move(
+                        TURN_LEFT);  //da Vorne, Rechts und Links Laserwerte anzeigen das Wände sehr dicht sind, umdrehen
             }
         }
-    }*/
     }
 
 
-    int main(int argc, char **argv) {
-        // Initialize a ROS node
-        ros::init(argc, argv, "node");
-        ros::NodeHandle n;
-        motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 100);
-        laser_subscriber = n.subscribe("/scan", 1000, laserCallback);
-        ros::Duration time_between_ros_wakeups(0.001);
 
-        while (ros::ok()) {
-            ros::spinOnce();
-            time_between_ros_wakeups.sleep();
+
+
+    /* for (size_t i = 0; i < range_size; i++) {
+         if (laser_ranges[i] < range_min) {
+             range_min = laser_ranges[i];
+         }*/
+
+
+    /*for (float x : msg->ranges) {  //geht die Laser Wertw in der For Schleife durch
+        if (x >= msg->range_min && x <= msg->range_max && x <=sensorEntfernung) { //wenn der x Wert größergleich ist als die min range und kleinergleich ist als die max range und
+            sensorEntfernung = x;     //kleiner gleich der SensorReichweite von 3,5 ist wird die sensorEntfernung auf x gesetzt
+        } else {        //ansonsten wird der Wert um 1 erhöht
+            counter += 1;
         }
-
-        return 0;
-
     }
+}*/
+}
+
+
+int main(int argc, char **argv) {
+    // Initialize a ROS node
+    ros::init(argc, argv, "node");
+    ros::NodeHandle n;
+    motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 100);
+    laser_subscriber = n.subscribe("/scan", 1000, laserCallback);
+    ros::Duration time_between_ros_wakeups(0.001);
+
+    while (ros::ok()) {
+        ros::spinOnce();
+        time_between_ros_wakeups.sleep();
+    }
+
+    return 0;
+
+}
